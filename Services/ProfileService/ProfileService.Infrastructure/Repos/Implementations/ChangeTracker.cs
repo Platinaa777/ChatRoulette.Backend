fc716049b95381df83b089565c4d40edb87336fb
denis <miroshnichenkodenis2004@gmail.com>
2024-03-30 01:56:34 +0300

    fix compile error with shared Id of aggregate roots

--- Services/ProfileService/ProfileService.Infrastructure/Repos/Implementations/ChangeTracker.cs
+++ Services/ProfileService/ProfileService.Infrastructure/Repos/Implementations/ChangeTracker.cs
@@ -1,4 +1,5 @@
 using System.Collections.Concurrent;
+using ProfileService.Domain.Models.Identity;
 using ProfileService.Domain.Shared;
 using ProfileService.Infrastructure.Repos.Interfaces;
 
@@ -6,14 +7,14 @@ namespace ProfileService.Infrastructure.Repos.Implementations;
 
 public class ChangeTracker : IChangeTracker
 {
-    public ConcurrentBag<AggregateRoot<Guid>> Entities { get; }
+    public ConcurrentBag<AggregateRoot<Id>> Entities { get; }
 
     public ChangeTracker()
     {
-        Entities = new ConcurrentBag<AggregateRoot<Guid>>();
+        Entities = new ConcurrentBag<AggregateRoot<Id>>();
     }
     
-    public void Track(AggregateRoot<Guid> entity)
+    public void Track(AggregateRoot<Id> entity)
     {
         Entities.Add(entity);
     }
--- Services/ProfileService/ProfileService.Infrastructure/Repos/Interfaces/IChangeTracker.cs
+++ Services/ProfileService/ProfileService.Infrastructure/Repos/Interfaces/IChangeTracker.cs
@@ -1,11 +1,12 @@
 using System.Collections.Concurrent;
+using ProfileService.Domain.Models.Identity;
 using ProfileService.Domain.Shared;
 
 namespace ProfileService.Infrastructure.Repos.Interfaces;
 
 public interface IChangeTracker
 {
-    ConcurrentBag<AggregateRoot<Guid>> Entities { get; }
+    ConcurrentBag<AggregateRoot<Id>> Entities { get; }
 
-    public void Track(AggregateRoot<Guid> entity);
+    public void Track(AggregateRoot<Id> entity);
 }
